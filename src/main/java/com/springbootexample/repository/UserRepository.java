8819bb71c1e7ab15b9536de48b91a99180d9a264
Vipul Kumar <vipul.singh@kiwitech.com>
2018-09-12 16:51:49 +0530

    named-parameterized queries

--- src/main/java/com/springbootexample/repository/UserRepository.java
+++ src/main/java/com/springbootexample/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NamedQuery;
 import java.util.List;
 
 @Repository
@@ -17,5 +18,4 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 
     @Query("select u from User u where u.id = :myID")
     public User printUserbyId(@Param("myID") Integer myId);
-
 }
