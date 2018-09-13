99670dd4e64dc8784eb5c7ec254e0d73f1a00b3c
Vipul Kumar <vipul.singh@kiwitech.com>
2018-09-13 16:22:42 +0530

    Added Derived Queries

--- src/main/java/com/springbootexample/repository/UserRepository.java
+++ src/main/java/com/springbootexample/repository/UserRepository.java
@@ -1,14 +1,11 @@
 package com.springbootexample.repository;
 
 import com.springbootexample.model.User;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.NamedQuery;
-import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
@@ -18,4 +15,9 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 
     @Query("select u from User u where u.id = :myID")
     public User printUserbyId(@Param("myID") Integer myId);
+
+    User findUserNameById(@Param("id") Integer id) ;
+
+    User findUserByName(@Param("name") String name) ;
+
 }
