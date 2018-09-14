ad2595396c4bcf06e37ea9e9c0fc931377155fcb
Vipul Kumar <vipul.singh@kiwitech.com>
2018-09-14 16:26:48 +0530

    #11 New config : UserApplication

--- src/main/java/com/springbootexample/config/UserApplication.java
+++ src/main/java/com/springbootexample/config/UserApplication.java
@@ -1,4 +1,35 @@
 package com.springbootexample.config;
 
-public class UserApplication {
+import com.springbootexample.model.UserAddress;
+import com.springbootexample.repository.UserAddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SuppressWarnings("unused")
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.springbootexample.*")
+@EntityScan(basePackages = "com.springbootexample.*")
+@EnableJpaRepositories(basePackages = {"com.springbootexample.*"})
+@SpringBootApplication
+public class UserApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserAddressRepository userAddressRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        UserAddress ua = new UserAddress(456, "Sector-16", "Noida", "UP", "Sec-16 Metro Station");
+        userAddressRepository.save(ua);
+        System.out.println("User Address Inserted Successfully");
+    }
 }
