b703010c1eedea25cd15f126da674326d023b4ab
Vipul Kumar <vipul.singh@kiwitech.com>
2018-09-14 11:47:56 +0530

    #3 Tests for Named Queries Examples (1 & 2)

--- src/test/java/com/springbootexample/testcases/BookApplicationTests.java
+++ src/test/java/com/springbootexample/testcases/BookApplicationTests.java
@@ -1,4 +1,44 @@
 package com.springbootexample.testcases;
 
+
+import com.springbootexample.config.BookApplication;
+import com.springbootexample.model.Book;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BookApplication.class)
 public class BookApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void contextLoads() {
+        System.out.println("\n--------------#0 contextLoads---------------------");
+        System.out.println("BookApplication Context Loaded Successfully");
+    }
+
+    @Test
+    public void findBooksTest() {
+        System.out.println("\n############### eg1-findBooksTest ###############");
+        List<Book> books = em.createNamedQuery("Book.findBooks", Book.class).getResultList();
+        for (Book b : books) {
+            System.out.println("ID : " + b.getId() + ", Name : " + b.getName());
+        }
+    }
+
+    @Test
+    public void findcategories() {
+        System.out.println("\n############### eg2-findcategories ###############");
+       List<Object[]> teasers = em.createNamedQuery("Book.categories").getResultList();
+       for (Object[] t : teasers)
+           System.out.println(t[0] + " - " +t[1] +" - " +t[2]);
+    }
 }
