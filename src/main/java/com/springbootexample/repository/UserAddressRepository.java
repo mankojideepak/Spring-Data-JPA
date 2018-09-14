3cc56a63f81451768f167d4255caeaabb2ba754a
Vipul Kumar <vipul.singh@kiwitech.com>
2018-09-14 16:27:58 +0530

    #12 New Repository : UserAddressRepository

--- src/main/java/com/springbootexample/repository/UserAddressRepository.java
+++ src/main/java/com/springbootexample/repository/UserAddressRepository.java
@@ -1,27 +1,11 @@
 package com.springbootexample.repository;
 
-import com.springbootexample.model.User;
-import org.springframework.data.jpa.repository.Query;
+import com.springbootexample.model.UserAddress;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-    @Query("select u.email from User u where u.name = ?1")
-    public String getEmailByName(String name);
-
-    @Query("select u from User u where u.id = :myID")
-    public User printUserbyId(Integer myId);
-
-    User findUserNameById(Integer id);
-
-    User findUserByName(String name);
-
-//    void deleteById(@Param("id") Integer id);
-
-    User findByEmail(String email);
+public interface UserAddressRepository extends CrudRepository<UserAddress, Integer> {
 
 }
