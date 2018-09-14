7ed7c0abdae3d06a2e53bd1680cb324258ac1f99
Vipul Kumar <vipul.singh@kiwitech.com>
2018-09-14 12:49:42 +0530

    #6 Changed records in Book Entity

--- src/main/java/com/springbootexample/config/BookApplication.java
+++ src/main/java/com/springbootexample/config/BookApplication.java
@@ -1,6 +1,8 @@
 package com.springbootexample.config;
 
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +10,43 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.springbootexample.*")
 @EntityScan(basePackages = "com.springbootexample.*")
 @EnableJpaRepositories(basePackages = {"com.springbootexample.*"})
 @SpringBootApplication
-public class BookApplication {
+public class BookApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
+    }
+
+//    @Autowired
+//    BookCategoryRepository bookCategoryRepository;
+
+    @Override
+    public void run(String... args) {
+////         ################ INSERTING VALUES INTO BOOKCATEGORY ENTITY ##################
+//        BookCategory categoryA = new BookCategory("Mystery");
+//        Set bookAs = new HashSet<Book>(){{
+//            add(new Book("Hound Of Baskerville", categoryA));
+//            add(new Book("Murder On Orient Express", categoryA));
+//            add(new Book("And Then There Were None", categoryA));
+//        }};
+//        categoryA.setBooks(bookAs);
+//
+//        BookCategory categoryB = new BookCategory("Philosophical");
+//        Set bookBs = new HashSet<Book>(){{
+//            add(new Book("The Secret", categoryB));
+//            add(new Book("The Magic", categoryB));
+//            add(new Book("The Power", categoryB));
+//        }};
+//        categoryB.setBooks(bookBs);
+//
+//        bookCategoryRepository.saveAll(new HashSet<BookCategory>() {{
+//            add(categoryA);
+//            add(categoryB);
+//        }});
     }
 }
