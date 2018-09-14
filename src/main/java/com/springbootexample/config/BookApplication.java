367eb7a3ccbebde0c64e54922cd7ce76e5e4d9cf
Vipul Kumar <vipul.singh@kiwitech.com>
2018-09-14 11:45:37 +0530

    #2 New config : BookApplication

--- src/main/java/com/springbootexample/config/BookApplication.java
+++ src/main/java/com/springbootexample/config/BookApplication.java
@@ -1,4 +1,21 @@
 package com.springbootexample.config;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.springbootexample.*")
+@EntityScan(basePackages = "com.springbootexample.*")
+@EnableJpaRepositories(basePackages = {"com.springbootexample.*"})
+@SpringBootApplication
 public class BookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+    }
 }
