23bc098bf9da6c81e3c8d820101f98a8069dee16
Vipul Kumar <vipul.singh@kiwitech.com>
2018-09-14 16:29:09 +0530

    #13 Custom Annotation Example

--- src/main/java/com/springbootexample/CustomAnnotations/MyArea.java
+++ src/main/java/com/springbootexample/CustomAnnotations/MyArea.java
@@ -1,4 +1,24 @@
 package com.springbootexample.CustomAnnotations;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Retention(RUNTIME)
+@Target({FIELD, ANNOTATION_TYPE, PARAMETER})
+@Constraint(validatedBy = AreaValidator.class)
 public @interface MyArea {
+    String message() default "Area should be Sector-16 only";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
